92dcf2702154c44f03019d78df3e8251f18d46fd
João Victor Maciel de Freitas <tr062510@comgas.com.br>
2026-02-05 19:45:50 -0300

    feat(3379-transformed-array): add solution and tests

--- LeetCode.Tests/EasyTests.cs
+++ LeetCode.Tests/EasyTests.cs
@@ -29,3 +29,16 @@ public class NimGameTests
         Assert.Equal(expected, result);
     }
 }
+
+public class TransformedArrayTests
+{
+    [Theory]
+    [InlineData(new int[] {3,-2,1,1}, new int[] {1,1,1,3})]
+    [InlineData(new int[] {-1,4,-1}, new int[] {-1,-1,4})]
+    public void Run_TransformedArray_Tests(int[] input, int[] expected)
+    {
+        var result = Problems.Easy.TransformedArray.Solution.ConstructTransformedArray(input);
+
+        Assert.Equal(expected, result);
+    }
+}
